87b68a0fa4d747b18ea7c3020443fe8272a7b852
Jason Stentz <jstentz@andrew.cmu.edu>
2022-12-26 14:06:47 -0500

    snake

--- lights/lights/animations/snake.py
+++ lights/lights/animations/snake.py
@@ -1,62 +1,65 @@
 from lights.lights_util import Lights
 import random
+from lights.constants import NUM_PIXELS, PIN, ORDER
 
-# make sure not to generate food that is inside of the snakes body
-# plays 1D snake!
-def snake(numFood=5):
-    food = random.sample(range(NUM_PIXELS), numFood)
-    body = [random.randint(0, NUM_PIXELS - 1)]
-    empty = set([i for i in range(NUM_PIXELS)])
-    empty.remove(body[0])
-    for f in food:
+def snake(pixels, numFood=5):
+  food = random.sample(range(NUM_PIXELS), numFood)
+  body = [random.randint(0, NUM_PIXELS - 1)]
+  empty = set([i for i in range(NUM_PIXELS)])
+  empty.remove(body[0])
+  for f in food:
+    empty.remove(f)
+  
+  while True:
+    # max length snake
+    if len(set(body)) == NUM_PIXELS:
+      food = random.sample(range(NUM_PIXELS), numFood)
+      body = [random.randint(0, NUM_PIXELS - 1)]
+      empty = set([i for i in range(NUM_PIXELS)])
+      empty.remove(body[0])
+      for f in food:
         empty.remove(f)
-    
-    while True:
-        # max length snake
-        if len(set(body)) == NUM_PIXELS:
-            food = random.sample(range(NUM_PIXELS), numFood)
-            body = [random.randint(0, NUM_PIXELS - 1)]
-            empty = set([i for i in range(NUM_PIXELS)])
-            empty.remove(body[0])
-            for f in food:
-                empty.remove(f)
-            # reset
+      # reset
 
-        # move the snake
-        head = body[0]
-        nearestFood = None
-        nearestDist = None
-        for i in food:
-            d = abs(i - head)
-            if nearestFood == None or d < nearestDist:
-                nearestFood = i
-                nearestDist = d
+    # move the snake
+    head = body[0]
+    nearestFood = None
+    nearestDist = None
+    for i in food:
+      d = abs(i - head)
+      if nearestFood == None or d < nearestDist:
+        nearestFood = i
+        nearestDist = d
 
-        dir = +1 if nearestFood > head else -1
-        newHead = head + dir
-        body.insert(0, newHead)
+    dir = +1 if nearestFood > head else -1
+    newHead = head + dir
+    body.insert(0, newHead)
 
-        if newHead in empty: 
-            empty.remove(newHead)
+    if newHead in empty: 
+      empty.remove(newHead)
 
-        if newHead in food:
-            food.remove(newHead)
-            foodOptions = list(empty)
-            if foodOptions != []:
-                food.append(random.choice(list(empty)))
-        else:
-            tail = body.pop()
-            empty.add(tail)
+    if newHead in food:
+      food.remove(newHead)
+      foodOptions = list(empty)
+      if foodOptions != []:
+        food.append(random.choice(list(empty)))
+    else:
+      tail = body.pop()
+      empty.add(tail)
 
-        # update pixels
-        for i in range(NUM_PIXELS):
-            if i in food:
-                pixels[i] = (255, 0, 0)
-            elif i == newHead:
-                pixels[i] = (0, 255, 0)
-            elif i in body:
-                pixels[i] = (0, 255, 0)
-            else:
-                pixels[i] = (0, 0, 0)    
-        pixels.show()
+    # update pixels
+    for i in range(NUM_PIXELS):
+      if i in food:
+        pixels[i] = (255, 0, 0)
+      elif i == newHead:
+        pixels[i] = (0, 255, 0)
+      elif i in body:
+        pixels[i] = (0, 255, 0)
+      else:
+        pixels[i] = (0, 0, 0)  
+    pixels.show()
+
+if __name__ == '__main__':
+  import argparse
+  pixels = Lights(PIN, NUM_PIXELS, auto_write=False, pixel_order=ORDER)
 
---
+++ lights/lights/constants.py
@@ -0,0 +1,6 @@
+import board
+import neopixel
+
+ORDER = neopixel.RGB
+NUM_PIXELS = 500
+PIN = board.D18
