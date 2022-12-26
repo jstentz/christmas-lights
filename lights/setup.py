ec5ff8ae864a6788fe617469a9faa62d00710dfc
Jason Stentz <jstentz@andrew.cmu.edu>
2022-12-26 01:42:44 -0500

    made lights a python package

---
+++ lights/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lights',
+      version='1.0',
+      description='Package for controlling christmas lights',
+      author='Jason Stentz',
+      author_email='jstentz@andrew.cmu.edu',
+      packages=['lights', 'lights/animations'],
+     )
