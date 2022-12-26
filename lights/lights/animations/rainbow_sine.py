655339da9e36be2b94a91598c955e86bcbad45f4
Jason Stentz <jstentz@andrew.cmu.edu>
2022-12-26 18:34:22 -0500

    solid color and rainbow_sine

---
+++ lights/lights/animations/rainbow_sine.py
@@ -0,0 +1,26 @@
+from lights.animations.base import BaseAnimation
+from lights.utils.colors import hsv_to_rgb
+import math
+
+class RainbowSine(BaseAnimation):
+  # exp must be an even number >= 2 and freq must be >= 1
+  def __init__(self, pixels, freq=1, exp=10, fps=None):
+    super().__init__(pixels, fps=fps)
+    self.freq = freq
+    self.exp = exp
+    self.t = 0
+    
+  def renderNextFrame(self):
+    NUM_PIXELS = len(self.pixels)
+    t = self.t
+    freq = self.freq
+    exp = self.exp
+    for i in range(NUM_PIXELS):
+      c = (i + t) % NUM_PIXELS
+      h = math.sin(i * math.pi / NUM_PIXELS)
+      s = 1.0
+      v = math.sin(c * math.pi * freq / NUM_PIXELS)**exp # change exponent for faster brightness drop off
+      self.pixels[i] = hsv_to_rgb(h, s, v)
+    self.t += 1
+        
+        
---
+++ lights/lights/animations/solid.py
@@ -0,0 +1,14 @@
+from lights.animations.base import BaseAnimation
+from lights.utils.colors import rgb_to_hsv, hsv_to_rgb
+
+class Solid(BaseAnimation):
+  def __init__(self, pixels, color=(255, 255, 255), brightness=1, fps=None):
+    super().__init__(pixels, fps=fps)
+    self.color = color
+    self.brightness = brightness
+    
+  def renderNextFrame(self):
+    h, s, _ = rgb_to_hsv(*self.color)
+    c = hsv_to_rgb(h, s, self.brightness)
+    self.pixels.fill(c)
+        
