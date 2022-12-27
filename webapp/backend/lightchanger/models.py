8cba6194be40465fc609c93f9c6856db5b9433c6
Tara Stentz <tarastentz@gmail.com>
2022-12-27 01:49:10 -0500

    Change model to add animation id

--- webapp/backend/lightchanger/models.py
+++ webapp/backend/lightchanger/models.py
@@ -6,6 +6,7 @@ class LightPatternOption(models.Model):
     title = models.TextField(max_length=100, default="")
     description = models.TextField(max_length=1000)
     image_url = models.URLField(null=True)
+    animation_id = models.TextField(default="")
 
 class LightPattern(models.Model):
     light_pattern_id = models.ForeignKey(LightPatternOption, on_delete=models.CASCADE)
