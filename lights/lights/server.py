bb5a3c28762543cab2206c1dc1abac998abd5fa0
Jason Stentz <jstentz@andrew.cmu.edu>
2022-12-27 12:14:20 -0500

    broken subprocess management

--- lights/lights/server.py
+++ lights/lights/server.py
@@ -3,16 +3,16 @@ from subprocess import Popen
 import pathlib
 from typing import Optional
 
-ID_TO_FILENAME = {
-  0: 'redlights.py',
-  1: 'greenlights.py',
-  2: 'snake.py',
-  3: 'snowflakes.py',
-  4: 'rainbow.py'
-}
+# ID_TO_FILENAME = {
+#   0: 'redlights.py',
+#   1: 'greenlights.py',
+#   2: 'snake.py',
+#   3: 'snowflakes.py',
+#   4: 'rainbow.py'
+# }
 
-BASE_PATH = pathlib.Path(__file__).parent
-ANIMATIONS_PATH = BASE_PATH / 'animations'
+# BASE_PATH = pathlib.Path(__file__).parent
+# ANIMATIONS_PATH = BASE_PATH / 'animations'
 
 p: Optional[Popen[bytes]] = None
 
@@ -20,17 +20,22 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def receive_data():
-  data = request.get_json()
-  print(data)
-  return 'Success!'
-
-@app.route('/select', methods=['GET'])
-def handle_select():
   global p
+  data = request.get_json()
+  animation = data['light_pattern_name']
   if p is not None:
     p.terminate()
-  p = Popen(['python', ANIMATIONS_PATH/'dummy.py'])
+    p.wait()
+  p = Popen(['python', 'run_animation.py', '-r', '-a', animation])
   return 'Success!'
+
+# @app.route('/select', methods=['GET'])
+# def handle_select():
+#   global p
+#   if p is not None:
+#     p.terminate()
+#   p = Popen(['python', ANIMATIONS_PATH/'dummy.py'])
+#   return 'Success!'
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=8000)
