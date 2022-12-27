286ae582f8436c186e1f7cd777cd421e281a261b
Ryan Stentz <rbot427@gmail.com>
2022-12-27 00:01:25 -0500

    start of readme, auto load animations

--- lights/lights/run_animation.py
+++ lights/lights/run_animation.py
@@ -1,8 +1,10 @@
 import ast
 import sys
 from typing import Dict
+import os
 
 # Animation imports.
+from lights.animations import ANIMATIONS
 from lights.animations.snake import Snake
 from lights.animations.snowflakes import Snowflakes
 from lights.animations.down_the_line import DownTheLine
@@ -17,26 +19,34 @@ from lights.animations.binary_count import BinaryCount
 
 from lights.animations.base import BaseAnimation
 
-ANIMATIONS: BaseAnimation = [
-  Snake, 
-  Snowflakes, 
-  DownTheLine, 
-  RainbowSine, 
-  ColorSine, 
-  MovingRainbow,
-  RedGreenSwap,
-  GameOfLife,
-  SingleColor,
-  ColorSort,
-  BinaryCount
-]
 NAME_TO_ANIMATION: Dict[str, BaseAnimation] = {animation.__name__: animation for animation in ANIMATIONS}
+
+def printExampleUsage(animation: BaseAnimation):
+  print("Example usage: python", os.path.basename(__file__), animation.exampleUsage())
 
 
 if __name__ == '__main__':
+  # Parse command line arguments.
+  animation_name = sys.argv[1]
+  animation = NAME_TO_ANIMATION[animation_name]
+  kwargs_start = 2
+  simulate = False
+  if len(sys.argv) >= 3 and sys.argv[2] == '-h':
+    printExampleUsage(animation)
+    exit(-1)
+  elif len(sys.argv) >= 3 and sys.argv[2] == '-s':
+    simulate = True
+    kwargs_start = 3
+  kwargs_list = [arg.split('=') for arg in sys.argv[kwargs_start:]]
+  kwargs = dict([(arg[0], ast.literal_eval(arg[1]))for arg in kwargs_list])
+
   # Setup.
+<<<<<<< HEAD
   SIMULATE = True
   if not SIMULATE:
+=======
+  if not simulate:
+>>>>>>> 8dad8b9 (start of readme, auto load animations)
     from neopixel import NeoPixel as LightsController
     from lights.constants import PIN, NUM_PIXELS, ORDER
   else:
@@ -44,19 +54,13 @@ if __name__ == '__main__':
     from lights.controller.lights_simulator import LightsSimulator
     LightsController = LightsSimulator
 
-  # Parse command line arguments.
-  animation_name = sys.argv[1]
-  kwargs_list = [arg.split('=') for arg in sys.argv[2:]]
-  kwargs = dict([(arg[0], ast.literal_eval(arg[1]))for arg in kwargs_list])
-
   # Run animation.
   pixels = LightsController(PIN, NUM_PIXELS, auto_write=False, pixel_order=ORDER)
-  animation = NAME_TO_ANIMATION[animation_name]
   
   try:
     a = animation(pixels, **kwargs)
   except Exception as e:
-    print("Example usage: python run_animation.py", animation.exampleUsage())
+    printExampleUsage(animation)
     raise e
 
   a.run()
