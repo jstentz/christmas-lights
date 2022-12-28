b4e9a3c53b0898e1486bc104adecd9571190dd21
Ryan Stentz <rbot427@gmail.com>
2022-12-28 09:50:39 -0500

    minor tweaks to the terminal simulator

--- lights/lights/controller/lights_simulator.py
+++ lights/lights/controller/lights_simulator.py
@@ -19,13 +19,13 @@ class TerminalLightsSimulator(BaseLightsSimulator):
     super().__init__(
       n, brightness=brightness, byteorder=pixel_order, auto_write=auto_write
     )
-    self.tcols, self.trows = os.get_terminal_size()
+    self.tcols, _ = os.get_terminal_size()
     os.system('cls||clear')
     cursor.hide()    
 
   def show(self):
-    num_rows_last_print = (self._pixels + self.tcols - 1) // self.tcols
-    buf = list(self._post_brightness_buffer)
+    self.tcols, _ = os.get_terminal_size()
+    num_rows_last_print = self._pixels // self.tcols
     s = ""
     for pixel in self:
       r, g, b = pixel[self._byteorder[0]], pixel[self._byteorder[1]], pixel[self._byteorder[2]]
@@ -34,9 +34,6 @@ class TerminalLightsSimulator(BaseLightsSimulator):
     # Push frame.
     print("\033[F" * num_rows_last_print, end="")
     print(s, end='', flush=True)
-
-    
-    self.tcols, self.trows = os.get_terminal_size()
 
   def __del__(self):
     cursor.show()
