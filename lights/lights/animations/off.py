46797ace08699861db092cab9dd1059ac2ea5f11
Ryan Stentz <rbot427@gmail.com>
2023-01-25 16:13:50 -0500

    low power off animation

--- lights/lights/animations/off.py
+++ lights/lights/animations/off.py
@@ -1,9 +1,12 @@
 from lights.animations.base import BaseAnimation
 
 class Off(BaseAnimation):
-  def __init__(self, pixels, *, color=(0,255,0), fps=None):
+  def __init__(self, pixels, *, fps=None):
     super().__init__(pixels, fps=fps)
     self.pixels.fill((0, 0, 0))
+    self.firstFrame = True
 
   def renderNextFrame(self):
-    pass
+    while self.running and not self.firstFrame:
+      pass
+    self.firstFrame = False
