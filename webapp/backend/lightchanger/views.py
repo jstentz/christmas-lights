806c5afeb04ca5efcf12cd9e3e60d1179595194d
Ryan Stentz <rbot427@gmail.com>
2023-12-08 19:46:30 -0500

    return a more informative error header

--- webapp/backend/lightchanger/views.py
+++ webapp/backend/lightchanger/views.py
@@ -24,7 +24,7 @@ def basic_authentication(func):
   @wraps(func)
   def wrapper(self, request, *args, **kwargs):
     if not is_request_authenticated(request):
-      return Response(status=401)
+      return Response(status=401, data="Unauthorized: Make sure you've scanned the qr-code at the base of the tree.")
     else:
       return func(self, request, *args, **kwargs)
   return wrapper
