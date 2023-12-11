2d7cedabc8ec36e579d778ede3003f372b7188e7
Jason Stentz <jstentz@andrew.cmu.edu>
2023-12-10 22:14:39 -0500

    added collatz animation

---
+++ lights/lights/animations/collatz.py
@@ -0,0 +1,37 @@
+from lights.animations.base import BaseAnimation
+from typing import Optional
+import random
+
+class Collatz(BaseAnimation):
+    def __init__(self, pixels, *, fps: Optional[int] = None, start_number: int = 499, decay: float = 0.85):
+        super().__init__(pixels, fps=fps)
+        self.number = random.randint(2, start_number)
+        self.start_number = start_number
+        self.decay = decay
+
+    def renderNextFrame(self):
+        NUM_PIXELS = len(self.pixels)
+        if self.number == 1:
+            self.number = random.randint(2, self.start_number)
+        current_number = self.number
+        decay = self.decay
+
+        # Apply Collatz conjecture
+        if current_number % 2 == 0:
+            current_number //= 2
+        else:
+            current_number = 3 * current_number + 1
+
+        # Map the current number to pixel index
+        index = current_number % NUM_PIXELS
+
+        # Turn on the corresponding pixel
+        self.pixels[index] = (255, 255, 255)
+
+        # Decay all pixels
+        for i in range(NUM_PIXELS):
+            color = self.pixels[i]
+            self.pixels[i] = tuple(int(c * decay) for c in color)
+
+        # Update the current number for the next frame
+        self.number = current_number
