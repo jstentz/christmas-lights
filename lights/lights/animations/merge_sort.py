d0850cfea84409ae855bacae3a884fc71ebb828d
Ryan Stentz <rbot427@gmail.com>
2023-12-15 19:23:54 -0500

    switch from brightness to hue

--- lights/lights/animations/merge_sort.py
+++ lights/lights/animations/merge_sort.py
@@ -7,22 +7,20 @@ import random
 
 class MergeSort(BaseAnimation):
   def __init__(self, pixels, *, 
-               fps: Optional[int] = 60, 
-               hue: int = 170, 
-               brightnessRange: Collection[int] = (0, 255), 
-               focusColor: Collection[int] = (255, 0, 0), 
+               fps: Optional[int] = 60,
+               hueRange: Collection[int] = (0, 50), 
+               focusColor: Collection[int] = (255, 255, 255), 
                finishColor: Collection[int] = (255, 255, 255), 
                parallel: bool = True,
                finishWaitTime: float = 3):
     super().__init__(pixels, fps)
-    self.hue = hue
-    self.brightnessRange = brightnessRange
+    self.hueRange = hueRange
     self.focusColor = focusColor
     self.finishColor = finishColor
     self.parallel = parallel
     self.finishFrames = int(self.fps * finishWaitTime) if self.fps is not None else 60 * finishWaitTime
     self.t = 0
-    self.colors = [(hue / 255, 1.0, b) for b in np.linspace(brightnessRange[0] / 255, brightnessRange[1] / 255, len(self.pixels))]
+    self.colors = [(b, 1.0, 1.0) for b in np.linspace(hueRange[0] / 255, hueRange[1] / 255, len(self.pixels))]
 
     if self.parallel:
       self.renderNextFrame = self.renderParallel
@@ -36,7 +34,7 @@ class MergeSort(BaseAnimation):
     self.mergers: Collection[Merger] = [Merger(l, l+1, l+2) for l in range(0, len(self.pixels), 2)]
     random.shuffle(self.colors)
     self.pixels[:] = [tuple(hsv_to_rgb(*hsv)) for hsv in self.colors]
-    self.brightnesses = [hsv[2] for hsv in self.colors]
+    self.brightnesses = [hsv[0] for hsv in self.colors]
     
   def renderParallel(self):
     if self.phase == "merge":
@@ -93,17 +91,14 @@ class MergeSort(BaseAnimation):
   def validate_parameters(cls, parameters):
     super().validate_parameters(parameters)
     full_parameters = {**cls.get_default_parameters(), **parameters}
-    hue = full_parameters['hue']
-    brightnessRange = full_parameters['brightnessRange']
+    hueRange = full_parameters['hueRange']
     focusColor = full_parameters['focusColor']
     finishColor = full_parameters['finishColor']
     # no need to validate parallel since it's already confirmed a boolean
     finishWaitTime = full_parameters['finishWaitTime']
 
-    if hue < 0 or hue > 255:
-      raise TypeError("hue must be a value in [0, 255]")
-    if not is_valid_inclusive_range(brightnessRange, 0, 255):
-      raise TypeError("brightnessRange must be a valid range tuple")
+    if not is_valid_inclusive_range(hueRange, 0, 255):
+      raise TypeError("hueRange must be a valid range tuple")
     if not is_valid_rgb_color(focusColor):
       raise TypeError("focusColor must be a valid rgb color")
     if not is_valid_rgb_color(finishColor):
@@ -123,14 +118,14 @@ class Merger:
       self.rColor = None
       self.init = False
       self.aux = [(0, 0, 0)] * (r - l)
-      self.brightnessAux = [0] * (r - l)
+      self.hueAux = [0] * (r - l)
 
     @classmethod
     def fromMergers(cls, merger1: "Merger", merger2: "Merger"):
       assert merger1.r == merger2.l
       return Merger(merger1.l, merger1.r, merger2.r)
     
-    def step(self, brightnesses, pixels, focusColor) -> bool:
+    def step(self, hues, pixels, focusColor) -> bool:
       if self.li >= self.m and self.ri >= self.r:
         return True
       
@@ -142,34 +137,34 @@ class Merger:
         self.init = True
       
       if self.li >= self.m:
-        self._advance_right(brightnesses, pixels, focusColor)
+        self._advance_right(hues, pixels, focusColor)
         return False
       if self.ri >= self.r:
-        self._advance_left(brightnesses, pixels, focusColor)
+        self._advance_left(hues, pixels, focusColor)
         return False
 
-      if brightnesses[self.li] <= brightnesses[self.ri]:
-        self._advance_left(brightnesses, pixels, focusColor)
+      if hues[self.li] <= hues[self.ri]:
+        self._advance_left(hues, pixels, focusColor)
       else:
-        self._advance_right(brightnesses, pixels, focusColor)
+        self._advance_right(hues, pixels, focusColor)
 
       return False
 
-    def finish(self, brightnesses, pixels, focusColor):
+    def finish(self, hues, pixels, focusColor):
       if self.fi >= len(self.aux):
         pixels[self.l + self.fi - 1] = self.aux[self.fi - 1]
-        brightnesses[self.l + self.fi - 1] = self.brightnessAux[self.fi - 1]
+        hues[self.l + self.fi - 1] = self.hueAux[self.fi - 1]
         return True
       
       pixels[self.l + self.fi] = focusColor
       pixels[self.l + self.fi - 1] = self.aux[self.fi - 1]
-      brightnesses[self.l + self.fi - 1] = self.brightnessAux[self.fi - 1]
+      hues[self.l + self.fi - 1] = self.hueAux[self.fi - 1]
       self.fi += 1
       return False
 
     def _advance_left(self, brightnesses, pixels, focusColor):
       self.aux[self.li - self.l + self.ri - self.m] = self.lColor
-      self.brightnessAux[self.li - self.l + self.ri - self.m] = brightnesses[self.li]
+      self.hueAux[self.li - self.l + self.ri - self.m] = brightnesses[self.li]
       pixels[self.li] = self.lColor
       self.li += 1
       if self.li < self.m:
@@ -177,7 +172,7 @@ class Merger:
 
     def _advance_right(self, brightnesses, pixels, focusColor):
       self.aux[self.li - self.l + self.ri - self.m] = self.rColor
-      self.brightnessAux[self.li - self.l + self.ri - self.m] = brightnesses[self.ri]
+      self.hueAux[self.li - self.l + self.ri - self.m] = brightnesses[self.ri]
       pixels[self.ri] = self.rColor
       self.ri += 1
       if self.ri < self.r:
