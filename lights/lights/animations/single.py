9cdfadecb522217c0860f09eb591aaa1f10648f5
Jason Stentz <jstentz@andrew.cmu.edu>
2023-12-18 16:59:51 -0500

    added animation for selecting a single light

---
+++ lights/lights/animations/single.py
@@ -0,0 +1,24 @@
+from lights.animations.base import BaseAnimation
+from typing import Optional, Collection
+from lights.utils.validation import is_valid_rgb_color
+
+class Single(BaseAnimation):
+  def __init__(self, pixels, *, fps: Optional[int] = None, color: Collection[int] = (230, 80, 255), light: int = 0):
+    super().__init__(pixels, fps=fps)
+    self.pixels.fill((0, 0, 0))
+    self.pixels[light] = color
+    
+  def renderNextFrame(self):
+    pass
+        
+  @classmethod
+  def validate_parameters(cls, parameters):
+    super().validate_parameters(parameters)
+    full_parameters = {**cls.get_default_parameters(), **parameters}
+    color = full_parameters['color']
+    light = full_parameters['light']
+
+    if not is_valid_rgb_color(color):
+      raise TypeError("color must be a valid rgb color tuple")
+    if light < 0 or light >= 500:
+      raise TypeError("light must be in the range [0, 500]")
