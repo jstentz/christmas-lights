ca42e73460f2916cf6436230bfafa965594fde71
Ryan Stentz <rbot427@gmail.com>
2023-12-25 22:47:09 -0500

    fix params

--- lights/lights/animations/down_the_plane.py
+++ lights/lights/animations/down_the_plane.py
@@ -5,7 +5,7 @@ from lights.utils.geometry import POINTS_3D
 
 class DownThePlane(BaseAnimation):
 
-  def __init__(self, frameBuf: np.ndarray, *, fps: Optional[int] = None, step: float = np.pi / 200, epsilon: float = 0.1, decay: float = 0.95):
+  def __init__(self, frameBuf: np.ndarray, *, fps: Optional[int] = None, step: float = np.pi / 64, epsilon: float = 0.1, decay: float = 0.95):
     super().__init__(frameBuf, fps)
     self.angle = 0
     self.step = step
