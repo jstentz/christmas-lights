68a8721cfc8cca4b665535fefb21ab6623215971
Ryan Stentz <rbot427@gmail.com>
2023-12-25 22:34:32 -0500

    fix typing

--- lights/lights/animations/down_the_plane.py
+++ lights/lights/animations/down_the_plane.py
@@ -1,10 +1,11 @@
 import numpy as np
+from typing import Optional
 from lights.animations.base import BaseAnimation
 from lights.utils.geometry import POINTS_3D
 
 class DownThePlane(BaseAnimation):
 
-  def __init__(self, frameBuf: np.ndarray, *, fps: int | None = None, step: float = np.pi / 200, epsilon: float = 0.1, decay: float = 0.95):
+  def __init__(self, frameBuf: np.ndarray, *, fps: Optional[int] = None, step: float = np.pi / 200, epsilon: float = 0.1, decay: float = 0.95):
     super().__init__(frameBuf, fps)
     self.angle = 0
     self.step = step
@@ -18,7 +19,7 @@ class DownThePlane(BaseAnimation):
     self.angle += self.step
     plane1 = np.array([np.sin(self.angle), np.cos(self.angle), 0])
     plane2 = np.array([np.sin(self.angle - np.pi/2), np.cos(self.angle - np.pi/2), 0])
-    
+
     dots1 = POINTS_3D @ plane1.reshape((-1, 1))
     dots2 = POINTS_3D @ plane2.reshape((-1, 1))
     colors[(np.abs(dots1) < self.epsilon).reshape((-1,))] = (255, 0, 0)
