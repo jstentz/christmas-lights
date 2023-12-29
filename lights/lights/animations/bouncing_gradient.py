6b9a294d087dad1f8aadf744fb1db45506757c46
Jason Stentz <jstentz@andrew.cmu.edu>
2023-12-28 23:18:00 -0500

    new animation

--- lights/lights/animations/bouncing_gradient.py
+++ lights/lights/animations/bouncing_gradient.py
@@ -1,13 +1,12 @@
 import numpy as np
+from matplotlib.colors import hsv_to_rgb
 from typing import Optional
 from lights.animations.base import BaseAnimation
 from lights.utils.geometry import POINTS_3D
 
-class BouncyBalls(BaseAnimation):
-  def __init__(self, frameBuf: np.ndarray, *, fps: Optional[int] = None, speed : float = 0.05, radius : float = 0.5):
+class BouncingGradient(BaseAnimation):
+  def __init__(self, frameBuf: np.ndarray, *, fps: Optional[int] = None, speed : float = 0.05):
     super().__init__(frameBuf, fps)
-    self.radius = radius
-
     # create the bounding box
     self.mins = np.min(POINTS_3D, axis=0)
     self.maxs = np.max(POINTS_3D, axis=0)
@@ -24,19 +23,20 @@ class BouncyBalls(BaseAnimation):
     self.loc += self.dir
 
     # find the dims where we are out of bounds
-    dims_min = self.loc < (self.mins + self.radius)
-    dims_max = self.loc > (self.maxs - self.radius)
+    dims_min = self.loc < self.mins
+    dims_max = self.loc > self.maxs
 
     # snap ball to min / max
-    self.loc[dims_min] = self.mins[dims_min] + self.radius
-    self.loc[dims_max] = self.maxs[dims_max] - self.radius
+    self.loc[dims_min] = self.mins[dims_min]
+    self.loc[dims_max] = self.maxs[dims_max]
 
     # change the direction where we collide
     self.dir[np.logical_or(dims_min, dims_max)] *= -1
 
-    # light up the lights accordingly
+    # light up lights based on distances
     distances = np.linalg.norm(POINTS_3D - np.expand_dims(self.loc, 0), axis=1)
-    lightup = distances < self.radius
 
-    self.frameBuf[lightup] = self.color
-    self.frameBuf[np.logical_not(lightup)] = np.zeros(3) + 20
+    hsv = np.ones((len(self.frameBuf), 3))
+    hsv[:, 0] = (np.max(distances) - distances) / np.max(distances)
+
+    self.frameBuf[:] = hsv_to_rgb(hsv) * 255
