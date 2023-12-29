054a92b044155ce115492495b237957acceeaaf2
Jason Stentz <jstentz@andrew.cmu.edu>
2023-12-28 22:33:27 -0500

    fasterer fire

--- lights/lights/animations/fire_fast.py
+++ lights/lights/animations/fire_fast.py
@@ -31,49 +31,86 @@ class FastFire(BaseAnimation):
     self.particles[:, 1] = 2.0 * self.particles[:, 1] - 0.5 
     self.particles[:, 2] = (self.particles[:, 2] - 0.5) * 0.1
 
+    # this can all be done in preprocessing
+    self.xs = POINTS_3D[:, 0]
+    self.zs = POINTS_3D[:, 1]
+    self.ys = POINTS_3D[:, 2]
+    self.thetas = np.arctan2(self.xs, self.zs)
+
+    self.new_xs = np.repeat(np.expand_dims(self.thetas, -1).T, self.num_particles, axis=0)
+    self.new_ys = np.repeat(np.expand_dims((self.ys - self.min_alt) / (self.max_alt - self.min_alt), -1).T, self.num_particles, axis=0)
+
   def renderNextFrame(self):
-    for i in range(len(POINTS_3D)):
-      coord = POINTS_3D[i]
-      color = self.get_colour_3d(coord[0], coord[1], coord[2])
+    
 
-      # convert to rgb from grb
-      self.frameBuf[i] = np.array([color[1], color[0], color[2]])
+    # these need to be repeated for all of the lights, so it should be 500 x 50
 
-    # do get_colour_3d but on all them at once
-    # colors = 
+    # need to create the brightness array, which is 500 x 1 (or just 500,)
 
-    self.step_particles()
+    # the result needs to be a 500 x 3 array
 
-  def get_colour(self, x, y):
-    result = y*0.5
-    # do value_at for all the particles at once
-    # values = np.copy(self.particles)
+    # up until here
+
+    result = (self.ys - self.min_alt) / (self.max_alt - self.min_alt)*0.5
     scale = self.particles[:, 1] + 1.0
-
-    dxs = (self.particles[:, 0] + np.pi - x) % (2.0 * np.pi) - np.pi
-    dys = self.particles[:, 1] - y
-    f = (dxs * dxs + dys * dys) / (self.radius_sq * scale * scale)
+    dxs = (np.expand_dims(self.particles[:, 0], -1) + np.pi - self.new_xs) % (2.0 * np.pi) - np.pi
+    dys = np.expand_dims(self.particles[:, 1], -1) - self.new_ys
+    f = (dxs * dxs + dys * dys) / np.expand_dims((self.radius_sq * scale * scale), -1)
 
     test = f < 1.0
     nottest = np.logical_not(test)
-    f[test] = (1.0 - f[test]**2) * self.particles[test, 1]
+    f[test] = (1.0 - f[test]**2) * self.new_ys[test]
     f[nottest] = 0.0
 
-    result += np.sum(f)
-    brightness = 1.0 - min(max(result-0.2, 0.0), 1.0)
-
-    # try basing the hue on brightness value? eh that's kinda hard
-
-    if brightness > 0.95:
-      return [self.max_brightness, self.max_brightness, (brightness-0.95)*self.max_brightness/0.05]
-    elif brightness > 0.85:
-      return [(brightness-0.85)*self.max_brightness/0.1, self.max_brightness, 0.0]
-    else:
-      return [0.0, brightness*self.max_brightness/0.85, 0.0]
+    result += np.sum(f, axis=0)
+    brightness = 1.0 - np.clip(result - 0.2, 0.0, 1.0)
+    condition1 = brightness > 0.95
+    condition2 = np.logical_and(brightness <= 0.95, brightness > 0.85)
+    condition3 = brightness <= 0.85
+    # self.frameBuf[condition1] = np.array([self.max_brightness, self.max_brightness, (brightness[condition1]-0.95)*self.max_brightness/0.05])
+    self.frameBuf[condition1, 0] = self.max_brightness
+    self.frameBuf[condition1, 1] = self.max_brightness
+    self.frameBuf[condition1, 2] = (brightness[condition1]-0.95)*self.max_brightness/0.05
     
-  def get_colour_3d(self, x, z, y):
-    theta = math.atan2(x, z)
-    return self.get_colour(theta, (y - self.min_alt)/(self.max_alt - self.min_alt))
+    self.frameBuf[condition2, 0] = (brightness[condition2]-0.85)*self.max_brightness/0.1
+    self.frameBuf[condition2, 1] = self.max_brightness
+    self.frameBuf[condition2, 2] = 0.0
+
+    self.frameBuf[condition3, 0] = 0.0
+    self.frameBuf[condition3, 1] = brightness[condition3]*self.max_brightness/0.85
+    self.frameBuf[condition3, 2] = 0.0
+
+    # self.frameBuf[condition2] = np.array([(brightness[condition2]-0.85)*self.max_brightness/0.1, self.max_brightness, 0.0])
+    # self.frameBuf[condition3] = np.array([0.0, brightness[condition3]*self.max_brightness/0.85, 0.0])
+    self.step_particles()
+
+  # def get_colour(self, x, y):
+  #   result = y*0.5
+  #   scale = self.particles[:, 1] + 1.0
+  #   dxs = (self.particles[:, 0] + np.pi - x) % (2.0 * np.pi) - np.pi
+  #   dys = self.particles[:, 1] - y
+  #   f = (dxs * dxs + dys * dys) / (self.radius_sq * scale * scale)
+
+  #   test = f < 1.0
+  #   nottest = np.logical_not(test)
+  #   f[test] = (1.0 - f[test]**2) * self.particles[test, 1]
+  #   f[nottest] = 0.0
+
+  #   result += np.sum(f)
+  #   brightness = 1.0 - min(max(result-0.2, 0.0), 1.0)
+
+  #   # try basing the hue on brightness value? eh that's kinda hard
+
+  #   if brightness > 0.95:
+  #     return [self.max_brightness, self.max_brightness, (brightness-0.95)*self.max_brightness/0.05]
+  #   elif brightness > 0.85:
+  #     return [(brightness-0.85)*self.max_brightness/0.1, self.max_brightness, 0.0]
+  #   else:
+  #     return [0.0, brightness*self.max_brightness/0.85, 0.0]
+    
+  # def get_colour_3d(self, x, z, y):
+  #   theta = math.atan2(x, z)
+  #   return self.get_colour(theta, (y - self.min_alt)/(self.max_alt - self.min_alt))
 
   def step_particles(self):
     # move the particles
