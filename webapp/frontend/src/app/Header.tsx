dc164649553d2deb77b23fb1a3de74616244e468
Ryan Stentz <rbot427@gmail.com>
2024-02-15 15:07:56 -0500

    minor ui changes

--- webapp/frontend/src/app/Header.tsx
+++ webapp/frontend/src/app/Header.tsx
@@ -17,13 +17,13 @@ export const Header: FC<Props> = ({
   return (
     <header className="bg-gray-800 p-2 pr-4 pl-4 flex justify-between items-center fixed w-full top-0 z-10">
       <HamburgerMenuIcon height={25} width={25} color="white" />
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center items-center">
         <p className="leading-tight max-w-fit text-xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-600 to-green-600">
             {title}
         </p>
         <p className="text-xs text-slate-200 text-center">
           Now Playing: <span> </span>
-          <Code color="sky">{selectedAnimationName}</Code>
+          <Code color="teal">{selectedAnimationName}</Code>
         </p>
       </div>
       <AlertDialog.Root>
