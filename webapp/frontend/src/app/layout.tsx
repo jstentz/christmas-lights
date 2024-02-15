bf26d7e0821e1a596345c832a61666d2dec5526c
Ryan Stentz <rbot427@gmail.com>
2024-02-14 23:48:45 -0500

    disable zooming

--- webapp/frontend/src/app/layout.tsx
+++ webapp/frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import '@radix-ui/themes/styles.css';
+import type { Viewport } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,12 @@ export const metadata: Metadata = {
   title: "Plaid Family Lights",
   description: "Web controller for the Plaid Family Lights",
 };
+
+export const viewport: Viewport = {
+  minimumScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false,
+}
 
 export default function RootLayout({
   children,
